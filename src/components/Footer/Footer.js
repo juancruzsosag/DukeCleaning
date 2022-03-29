b81cdb34e83eb5d67aa9f268804914f72a23bb51
juancruzsosagab <juancruzsosa@andesmar.com.ar>
2022-03-28 22:55:24 -0300

    My components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import "./Header.scss";
+import React from "react"; 
+
+
+
+export const Header = () =>{
+    return(
+        <div>
+        </div>
+    );
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Footer.scss";
+import React from "react"; 
+
+
+
+export const Footer = () =>{
+    return(
+        <div>
+        </div>
+    );
+}
