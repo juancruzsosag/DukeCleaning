655744b316140fe206438227f14dc402390fd7ac
ValentinaTarapow <tarapow.v@gmail.com>
2022-03-30 19:49:16 -0300

    Whatsapp Icon (no pop up yet)

--- src/components/WhatsApp/WhatsApp.js
+++ src/components/WhatsApp/WhatsApp.js
@@ -1,11 +1,15 @@
 import "./WhatsApp.scss";
-import React from "react"; 
+import React from "react";
+import WppIcon from "../WhatsApp/wpp-corner.svg";
 
 
 
 export const WhatsApp = () =>{
     return(
-        <div>
+        <div id="fixedTab">
+            <a href="" title="WhatsApp" class="d-flex justify-content-end"> 
+                <img src={WppIcon} class="w-75" alt="whatsapp" id="img-whatsapp"/>
+            </a>
         </div>
     );
 }
