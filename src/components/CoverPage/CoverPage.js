fcc7afec18ae4e586216e1eabc6884eef0d9b494
juancruzsosagab <juancruzsosa@andesmar.com.ar>
2022-03-29 21:25:17 -0300

    [header,coverpage]

--- src/components/CoverPage/CoverPage.js
+++ src/components/CoverPage/CoverPage.js
@@ -1,11 +1,31 @@
 import "./CoverPage.css";
-import React from "react"; 
+import React from "react";
+import Carousel from 'react-bootstrap/Carousel'
 
-
-
-export const CoverPage = () =>{
-    return(
-        <div className="">
-        </div>
+export const CoverPage = () => {
+    return (
+        <Carousel fade controls={false} indicators={false}>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 carousel-img"
+                    src="./images/CoverPage1.jpg"
+                    alt="First slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 carousel-img"
+                    src="./images/CoverPage2.jpg"
+                    alt="Second slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 carousel-img"
+                    src="./images/CoverPage3.jpg"
+                    alt="Third slide"
+                />
+            </Carousel.Item>
+        </Carousel>
     );
 }
