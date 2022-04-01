c13fefea5f9e327b65bda796b8e1b38c07b22d7a
ValentinaTarapow <tarapow.v@gmail.com>
2022-04-01 16:59:29 -0300

    Fix error: class -> className

--- src/components/WhatsApp/WhatsApp.js
+++ src/components/WhatsApp/WhatsApp.js
@@ -7,8 +7,8 @@ import WppIcon from "../WhatsApp/wpp-corner.svg";
 export const WhatsApp = () =>{
     return(
         <div id="fixedTab">
-            <a href="" title="WhatsApp" class="d-flex justify-content-end"> 
-                <img src={WppIcon} class="w-75" alt="whatsapp" id="img-whatsapp"/>
+            <a href="" title="WhatsApp" className="d-flex justify-content-end"> 
+                <img src={WppIcon} className="w-75" alt="whatsapp" id="img-whatsapp"/>
             </a>
         </div>
     );
