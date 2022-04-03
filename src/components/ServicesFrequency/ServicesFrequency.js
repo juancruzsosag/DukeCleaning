125143da8b8bb1ee990eff4797c71729f133f3ea
ValentinaTarapow <tarapow.v@gmail.com>
2022-04-03 16:06:34 -0300

    Sections development: services, frequency

--- src/components/ServicesFrequency/ServicesFrequency.js
+++ src/components/ServicesFrequency/ServicesFrequency.js
@@ -1,11 +1,28 @@
 import "./ServicesFrequency.scss";
 import React from "react"; 
-
-
+import { FrequencyItem } from "./FrequencyItem";
+import TwiceMonth from "./FrequencyImg/twoPerMonth.svg";
+import TwiceWeek from "./FrequencyImg/twoPerWeek.svg";
+import OneTime from "./FrequencyImg/oneTime.svg";
+import Daily from "./FrequencyImg/daily.svg";
+import Weekly from "./FrequencyImg/weekly.svg";
 
 export const ServicesFrequency = () =>{
     return(
-        <div>
+        <div id="ServicesFrequency">
+            <h2>Our team adapts to cover your <span>specific needs</span>.</h2>
+            <div className="frequency-container">
+                <div className="frq-first-row">
+                    <FrequencyItem title="Daily" img={Daily}  />
+                    <FrequencyItem title="Twice a week" img={TwiceWeek}  />
+                    <FrequencyItem title="Weekly" img={Weekly}  />
+                </div>
+                <div className="frq-second-row">
+                    <FrequencyItem title="Twice a month" img={TwiceMonth}  />
+                    <FrequencyItem title="One-time deep cleaning" img={OneTime}  />
+                </div>
+
+            </div>
         </div>
     );
 }
