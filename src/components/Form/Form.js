e2b17f0ee4450c3887489722560012272f674b18
ValentinaTarapow <tarapow.v@gmail.com>
2022-04-07 16:46:40 -0300

    Form progress (not ready)

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,18 +1,34 @@
 import "./Form.scss";
-import React from "react"; 
-import formPhoto from "./formPhoto.svg";
+import React , { useRef } from "react"; 
+import formPhoto from "../../assets/formPhoto.svg";
 import {IoPaperPlane} from "react-icons/io5";
+import emailjs from '@emailjs/browser';
+import { validateForm } from '../../helpers/validateForm'
 
 
 export const Form = () =>{
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_USER_ID')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+        });
+    };
+
     return(
         <div id="formSection">
             <div>
                 <h2 id="section-title">Give us a call or fill<br/> out the form <span>today</span>!</h2>
-                <img src={formPhoto} />
+                <img src={formPhoto} alt="Cleaning team illustration" />
             </div>
 
-            <form>
+            <form ref={form} onSubmit={sendEmail}>
                 <p className="form-title">Get your cleaning quote</p>
                 <input
                     name="company-name"
