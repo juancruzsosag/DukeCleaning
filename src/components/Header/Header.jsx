c20310a664eeb341faadace0b288801b20dea9d4
juancruzsosagab <juancruzsosa@andesmar.com.ar>
2022-04-08 00:56:26 -0300

    Avances

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,47 @@
-import "./Header.css";
+import "./Header.scss";
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
-
+import { IoLogoWhatsapp } from "react-icons/io";
+import { IoCall } from "react-icons/io5";
 import { Container, Nav, Row, Col } from "react-bootstrap";
 
 export const Header = () => {
   return (
     <Navbar>
-      <Container  className="navbar-header">
+      <Container fluid className="navbar-header">
         <Row>
-          <Col className="display-none"></Col>
-          <Col className="header-img">
-            <Nav  >
-              <Navbar.Brand  href="#home">
+          <Col xxl={4} className=" display-none" style={{
+          backgroundColor: 'red',
+        }}></Col>
+          <Col xxl={4} className="header-img" style={{
+          backgroundColor: 'green',
+        }}>
+            <Nav>
+              <Navbar.Brand href="#home">
                 <img src="./images/thedukerey-logo.svg" alt="" srcset="" />
               </Navbar.Brand>
             </Nav>
           </Col>
-          <Col className="display-none">
-            <Nav >
-              <Nav.Link  href="#action1">
+          <Col xxl={4} className="display-none" style={{
+          backgroundColor: 'yellow',
+        }}>
+            <Nav>
+              <Nav.Link href="#action1">
                 <Button className="navbar-wpbutton">
-                  <div className="whatsapp-logo"></div>WhatsApp
+                  <span className="whatsapp-logo">
+                    <IoLogoWhatsapp />
+                  </span>{"  "}
+                  WhatsApp
                 </Button>
               </Nav.Link>
-              <Nav.Link  href="#action1">
-                <Button className="navbar-callbutton">Call us</Button>
+              <Nav.Link href="#action1">
+                <Button className="navbar-callbutton">
+                  <span className="call-logo">
+                    <IoCall />
+                  </span>{"  "}
+                  Call us
+                </Button>
               </Nav.Link>
             </Nav>
           </Col>
