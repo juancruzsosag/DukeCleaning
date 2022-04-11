afb538e7106b6d332db1350237ce8d28e649669e
ValentinaTarapow <tarapow.v@gmail.com>
2022-04-11 03:28:03 -0300

    Form

--- src/helpers/validateForm.js
+++ src/helpers/validateForm.js
@@ -1,43 +1,4 @@
-// import Swal from "sweetalert2"
-// import "./helperStyling.scss"
 
+export const validateData = (values) => {
 
-// export const validateData = (values) => {
-
-//     if (values.name.length < 2) {
-//         Swal.fire({
-//             icon: 'error',
-//             title: 'Invalid name',
-//             text: 'You must complete the name field (at least 2 character)'
-//         })
-//         return false
-//     }
-
-//     if (values.surname.length < 2) {
-//         Swal.fire({
-//             icon: 'error',
-//             title: 'Invalid surname',
-//             text: 'You must complete the surname field (at least 2 character)'
-//         })
-//         return false
-//     }
-
-//     if (values.email.length < 5) {
-//         Swal.fire({
-//             icon: 'error',
-//             title: 'Invalid email',
-//             text: 'You must complete the surname field (at least 6 characters)'
-//         })
-//         return false
-//     }
-
-//     if (values.emailConfirm !== values.email) {
-//         Swal.fire({
-//             icon: 'error',
-//             title: 'Emails do not match'
-//         })
-//         return false
-//     }
-
-//     return true
-// }
+}
