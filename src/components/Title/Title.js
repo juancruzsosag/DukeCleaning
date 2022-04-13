85c9fea3019c111ccebf87cec8400ec2f0bb1d3b
ValentinaTarapow <tarapow.v@gmail.com>
2022-04-12 21:58:21 -0300

    Open Graph Protocol

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,4 +1,5 @@
 import Helmet from "react-helmet"
+import Logo from "../../assets/messagelogo.svg"
 
 export const Title = () => {
 
@@ -7,6 +8,10 @@ export const Title = () => {
             <meta charSet="utf-8" />
             <title>The Duke Rey: cleaning services</title>
             <link rel="canonical" href="http://mysite.com/example" />
+            <meta name="og:title" content="The Duke Rey: cleaning services" data-react-helmet="true" />
+            <meta name="og:description" content="The best commercial cleaning services in Miami
+            We focus on the cleaning of kitchens, restaurants and reatils stores." data-react-helmet="true" />
+            <meta name="og:image" content={Logo} data-react-helmet="true" />
         </Helmet>
 
     )
