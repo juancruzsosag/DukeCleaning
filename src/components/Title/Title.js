824a0b401d687934b4c79abb5c55eddac91c8754
ValentinaTarapow <tarapow.v@gmail.com>
2022-04-27 03:29:27 -0300

    Title / Open Graph changes

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -6,12 +6,12 @@ export const Title = () => {
     return (
         <Helmet>
             <meta charSet="utf-8" />
-            <title>The Duke Rey: cleaning services</title>
+            <title>The Duke Rey Cleaning</title>
             <link rel="canonical" href="http://mysite.com/example" />
             <meta name="og:title" content="The Duke Rey Cleaning" data-react-helmet="true" />
             <meta name="og:description" content="The best commercial cleaning services in Miami
             We focus on the cleaning of kitchens, restaurants and reatils stores." data-react-helmet="true" />
-            <meta name="og:image" content={OpenGraphLogo} data-react-helmet="true" />
+            <meta name="og:image" href={OpenGraphLogo} data-react-helmet="true" />
         </Helmet>
 
     )
