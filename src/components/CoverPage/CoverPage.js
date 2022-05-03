8c1525e8710c5891cbe74913368f0a7b6fd7da42
juancruzsosagab <juancruz.sosag@gmail.com>
2022-05-03 20:43:50 -0300

    ajustes

--- src/components/CoverPage/CoverPage.js
+++ src/components/CoverPage/CoverPage.js
@@ -4,6 +4,9 @@ import Carousel from 'react-bootstrap/Carousel'
 import CoverPage1 from "../../assets/images/CoverPage1.jpg"
 import CoverPage2 from "../../assets/images/CoverPage2.jpg"
 import CoverPage3 from "../../assets/images/CoverPage3.jpg"
+import CoverPage1Desk from "../../assets/images/CoverPage1-desktop.jpg"
+import CoverPage2Desk from "../../assets/images/CoverPage2-desktop.jpg"
+import CoverPage3Desk from "../../assets/images/CoverPage3-desktop.jpg"
 
 
 export const CoverPage = () => {
@@ -13,21 +16,21 @@ export const CoverPage = () => {
             <Carousel.Item>
                 <img
                     className="d-block w-100 "
-                    src={CoverPage1}
+                    src={screen.width>600 ? CoverPage1Desk : CoverPage1}
                     alt="First slide"
                 />
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src={CoverPage2}
+                    src={screen.width>600 ? CoverPage2Desk : CoverPage2}
                     alt="Second slide"
                 />
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src={CoverPage3}
+                    src={screen.width>600 ? CoverPage3Desk : CoverPage3}
                     alt="Third slide"
                 />
             </Carousel.Item>
