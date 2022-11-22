02c9b289e4bbbc03fe447e432f5b0ff1fa7c3f79
juancruzsosagab <juancruz.sosag@gmail.com>
2022-11-21 23:02:57 -0300

    Fix sintax error in cover description

--- src/components/CoverDescription/CoverDescription.js
+++ src/components/CoverDescription/CoverDescription.js
@@ -7,7 +7,7 @@ export const CoverDescription = () =>{
     return(
         <div id="coverDescription">
             <h1>The best commercial cleaning services in Miami</h1>
-            <h2>We focus on the cleaning of kitchens, restaurants and reatils stores.</h2>
+            <h2>We focus on the cleaning of kitchens, restaurants and retail stores.</h2>
         </div>
     );
 }
